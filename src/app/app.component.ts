b9aab8a446936c42b46f87516bff40b8e416f1b2
Tracy Silverio <tiritord@gmail.com>
2023-06-23 21:26:51 -0400

    juego de Dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dados';
+  dado1='../assets/img/dice1.png';
+  dado2='../assets/img/dice2.png';
+  numero1:number=0;
+  numero2:number=0;
+
+  cambiar():void{
+    this.numero1=Math.round(Math.random()*5) + 1;
+    this.numero2=Math.round(Math.random()*5) + 1;
+this.dado1='../assets/img/dice' + this.numero1 + '.png';
+this.dado2='../assets/img/dice' + this.numero2 + '.png';
+ 
+  }
+
+
+
+
 }
